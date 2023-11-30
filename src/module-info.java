8ef8aabf195815cf71018dd1a341b02dc7ab6c65
Jackson Casey <65034067+jaxcksn@users.noreply.github.com>
2023-11-30 15:50:11 -0600

    Fixed module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -3,7 +3,6 @@ module GroupProject {
     requires javafx.graphics;
     requires javafx.fxml;
     requires javafx.base;
-    requires org.json;
     requires java.desktop;
     opens FlightReservationSystem to javafx.graphics, javafx.fxml, javafx.base;
     opens FlightReservationSystem.GUI to javafx.fxml, javafx.graphics, javafx.base;
